1f45e622d5508bc8ffee0849e0debdc39dd562db
NeuroStar-88 <32369010+NeuroStar-88@users.noreply.github.com>
2019-05-17 14:11:35 -0400

    Add files via upload
    This is a simple file, additions for the code is added.

---
+++ Board.py
@@ -0,0 +1,15 @@
+board = [[0,1,0,1,0,1,0,1],
+         [1,0,1,0,1,0,1,0]]
+l = " a, b, c, d, e, f, g, h"
+count = 0
+
+print(l)
+
+for row in range(4):
+    while count < 8:
+        print(board[0])
+        print(board[1])
+        count += 1
+        print(count)
+
+
